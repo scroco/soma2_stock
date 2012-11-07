da393f181be278ca2e3c155d4a3562251c7ae9b5
LEETAEHO <tae1560@gmail.com>
2012-11-07 13:55:23 +0900

    Added schema association with stock_code and day_candle

--- app/models/day_candle.rb
+++ app/models/day_candle.rb
@@ -4,4 +4,6 @@ class DayCandle < ActiveRecord::Base
   def self.duplicated? symbol, date
     self.where(:symbol => symbol, :date => date).exists?
   end
+
+  has_many :stock_codes, :primary_key => :symbol, :foreign_key => :symbol
 end
