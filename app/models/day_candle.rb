bd8eae09399213160e8550af76cfdb22699e7e1f
tprpc <tprpc@tprpc.com>
2012-11-20 21:25:53 +0900

    belongs to error 처리

--- app/models/day_candle.rb
+++ app/models/day_candle.rb
@@ -1,10 +1,10 @@
 class DayCandle < ActiveRecord::Base
-  attr_accessible :symbol, :date, :o, :h, :l, :c, :v, :trading_date
+  attr_accessible :symbol, :date, :o, :h, :l, :c, :v
 
   def self.duplicated? symbol, date
     self.where(:symbol => symbol, :date => date).exists?
   end
 
-  belongs_to :firm_daily_data
+  belongs_to :firm_daily_datum
   belongs_to :stock_code, :primary_key => :symbol, :foreign_key => :symbol
 end
