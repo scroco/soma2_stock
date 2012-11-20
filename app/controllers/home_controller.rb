2d13d02f4e123ba1e623ef3b6eed06a6eef920e1
tprpc <tprpc@tprpc.com>
2012-11-21 04:51:04 +0900

    view and website migration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @msg = "blabla"
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ Stock2::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  match '/ajax/DayCandles/:symbol' => 'DayCandles#show'
+  #match '/ajax/DayCandles/:symbol' => 'DayCandles#show'
 end
